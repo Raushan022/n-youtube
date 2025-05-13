5f706e144284b21fe773fee24e01beb2a6d352b6
raushan <kraushan022@gmail.com>
2025-05-13 16:18:27 +0530

    added comments

---
+++ src/components/CommentsContainer.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+
+const commentsData = [
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [
+          {
+            name: "Akshay Saini",
+            text: "Lorem ipsum dolor sit amet, consectetur adip",
+            replies: [
+              {
+                name: "Akshay Saini",
+                text: "Lorem ipsum dolor sit amet, consectetur adip",
+                replies: [
+                  {
+                    name: "Akshay Saini",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [
+                      {
+                        name: "Akshay Saini",
+                        text: "Lorem ipsum dolor sit amet, consectetur adip",
+                        replies: [],
+                      },
+                    ],
+                  },
+                  {
+                    name: "Akshay Saini",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+];
+
+const Comment = ({ data }) => {
+  return (
+    <div className="flex shadow-sm bg-gray-100 rounded-lg my-2">
+      <img
+        className="w-12 h-12 "
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8MfyMJRMS7x4aUcKFgh5o_DS_MR-BQcdDjcf7zzBVo5MYrDRVWw&s=10&ec=72940544"
+        alt="user-icon"
+      />
+      <div className="px-3">
+        <p className="font-bold">{data.name}:</p>
+        <p>{data.text}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentList = ({ comments }) => {
+  return comments.map((comment, index) => (
+    <>
+      <Comment key={index} data={comment} />
+      <div className="pl-1 m-5">
+        <CommentList comments={comment.replies} />
+      </div>
+    </>
+  ));
+};
+
+const CommentsContainer = () => {
+  return (
+    <div className="m-5 p-2">
+      <h1 className="text-2xl font-bold">Comments</h1>
+      <CommentList comments={commentsData} />
+    </div>
+  );
+};
+
+export default CommentsContainer;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const GOOGLE_API_KEY = 'AIzaSyCZ_ACDpZgUn_T-vUyZYY6PpvgqT9r3u9g'
 
-export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=' + GOOGLE_API_KEY;
+export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=PK&key=' + GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
